d7bc9a11e7ccee4d8c293fd0f15eab36477691ba
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-12 18:16:14 +0000

    checking the build names

--- src/main/java/com/estafet/openshift/boost/console/api/jenkins/service/StateService.java
+++ src/main/java/com/estafet/openshift/boost/console/api/jenkins/service/StateService.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,8 @@ import com.openshift.restclient.model.IBuild;
 @Service
 public class StateService {
 
+	private static final Logger log = LoggerFactory.getLogger(StateService.class);
+	
 	@Autowired
 	private OpenShiftClient client;
 
@@ -47,9 +51,11 @@ public class StateService {
 	private Map<String, IBuild> latestBuilds() {
 		Map<String, IBuild> builds = new HashMap<String, IBuild>();
 		for (IBuild build : client.getBuilds()) {
+			log.info("checking build - " + build.getName());
 			String buildName = BuildUtil.buildName(build);
 			if (!builds.keySet().contains(buildName)
 					|| BuildUtil.buildDate(build).after(BuildUtil.buildDate(builds.get(buildName)))) {
+				log.info("adding build - " + buildName);
 				builds.put(buildName, build);
 			}
 		}
