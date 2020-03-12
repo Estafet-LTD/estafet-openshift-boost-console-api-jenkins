625006d0a5c19e9b311778b7997ac8a6ccba5ced
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-12 16:52:34 +0000

    Enabled jms consumer with opentracing

--- src/main/java/com/estafet/openshift/boost/console/api/jenkins/Application.java
+++ src/main/java/com/estafet/openshift/boost/console/api/jenkins/Application.java
@@ -1,14 +1,25 @@
 package com.estafet.openshift.boost.console.api.jenkins;
 
+import javax.jms.ConnectionFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.SimpleMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import io.opentracing.Tracer;
+import io.opentracing.contrib.jms.spring.TracingJmsTemplate;
 
 @Configuration
 @ComponentScan
@@ -30,6 +41,26 @@ public class Application extends SpringBootServletInitializer {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
 		return restTemplateBuilder.build();
+	}
+	
+	@Bean
+	public MessageConverter tracingJmsMessageConverter(Tracer tracer) {
+		return new PropagatingTracingMessageConverter(new SimpleMessageConverter(), tracer);
+	}
+
+	@Bean
+	public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory, Tracer tracer) {
+		JmsTemplate jmsTemplate = new TracingJmsTemplate(connectionFactory, tracer);
+		jmsTemplate.setMessageConverter(new SimpleMessageConverter());
+		return jmsTemplate;
+	}
+
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		configurer.configure(factory, connectionFactory);
+		return factory;
 	}
 	
 }
