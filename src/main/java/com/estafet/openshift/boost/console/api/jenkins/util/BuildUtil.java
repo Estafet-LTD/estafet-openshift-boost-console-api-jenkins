9051dd15ffa76c30a8765b2e3d1a4ebf3043cbd4
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-12 17:37:22 +0000

    fixed the matcher

--- src/main/java/com/estafet/openshift/boost/console/api/jenkins/util/BuildUtil.java
+++ src/main/java/com/estafet/openshift/boost/console/api/jenkins/util/BuildUtil.java
@@ -1,31 +1,27 @@
 package com.estafet.openshift.boost.console.api.jenkins.util;
 
 import java.util.Date;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.estafet.openshift.boost.commons.lib.date.DateUtils;
 import com.openshift.restclient.model.IBuild;
 
 public class BuildUtil {
 
-	private static final Logger log = LoggerFactory.getLogger(BuildUtil.class);
-	
 	private static Pattern pattern = Pattern.compile("(.+)(\\-\\d+)");
 
-	private BuildUtil( ) { }
+	private BuildUtil() {
+	}
 
 	public static Date buildDate(IBuild build) {
 		return DateUtils.getDate(build.getCreationTimeStamp());
 	}
 
 	public static String buildName(IBuild build) {
-		log.info("build - " + build.getName());
-		return pattern.matcher(build.getName()).group(1);
+		Matcher matcher = pattern.matcher(build.getName());
+		matcher.find();
+		return matcher.group(1);
 	}
-	
-	
-	
+
 }
