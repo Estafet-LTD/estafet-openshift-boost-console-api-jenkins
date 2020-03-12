0476eea249d8f85bb47d6c2f598b9582635cad9a
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-12 17:31:55 +0000

    diagnostics for the build name

--- src/main/java/com/estafet/openshift/boost/console/api/jenkins/util/BuildUtil.java
+++ src/main/java/com/estafet/openshift/boost/console/api/jenkins/util/BuildUtil.java
@@ -3,11 +3,16 @@ package com.estafet.openshift.boost.console.api.jenkins.util;
 import java.util.Date;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.estafet.openshift.boost.commons.lib.date.DateUtils;
 import com.openshift.restclient.model.IBuild;
 
 public class BuildUtil {
 
+	private static final Logger log = LoggerFactory.getLogger(BuildUtil.class);
+	
 	private static Pattern pattern = Pattern.compile("(.+)(\\-\\d+)");
 
 	private BuildUtil( ) { }
@@ -17,6 +22,7 @@ public class BuildUtil {
 	}
 
 	public static String buildName(IBuild build) {
+		log.info("build - " + build.getName());
 		return pattern.matcher(build.getName()).group(1);
 	}
 	
