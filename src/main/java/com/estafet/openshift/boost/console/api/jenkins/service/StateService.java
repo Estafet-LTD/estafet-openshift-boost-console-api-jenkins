204bdb81bc6ec069863a91f75c45076dc345d646
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-14 22:10:30 +0000

    Check both comparisons for the builds

--- src/main/java/com/estafet/openshift/boost/console/api/jenkins/service/StateService.java
+++ src/main/java/com/estafet/openshift/boost/console/api/jenkins/service/StateService.java
@@ -50,8 +50,9 @@ public class StateService {
 		for (IBuild build : client.getBuilds()) {
 			log.info("checking build - " + build.getName());
 			String buildName = BuildUtil.buildName(build);
-			if (!builds.keySet().contains(buildName) || (BuildUtil.isValidDate(build)
-					&& BuildUtil.buildDate(build).after(BuildUtil.buildDate(builds.get(buildName))))) {
+			if (!builds.keySet().contains(buildName)
+					|| (BuildUtil.isValidDate(build) && BuildUtil.isValidDate(builds.get(buildName))
+							&& BuildUtil.buildDate(build).after(BuildUtil.buildDate(builds.get(buildName))))) {
 				log.info("adding build - " + buildName);
 				builds.put(buildName, build);
 			}
